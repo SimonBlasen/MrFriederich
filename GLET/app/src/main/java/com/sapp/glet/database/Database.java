31831de97a330e1e1be7637d73a81c6e87233106
SimonBlasen <simon.yzf@t-online.de>
2017-03-04 09:28:09 +0100

    Added Database and some small dummy implementation

---
+++ GLET/app/src/main/java/com/sapp/glet/database/Database.java
@@ -0,0 +1,36 @@
+package com.sapp.glet.database;
+
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Simon on 04.03.2017.
+ */
+
+public class Database {
+
+    private static List<Player> m_players = new ArrayList<Player>();
+
+    /**
+     * Returns the current amount of players available in total
+     * @return The amount of players
+     */
+    public static int getPlayersAmount() {
+        return m_players.size();
+    }
+
+    /**
+     * Returns the requested player or null, if the index is out of the lists range
+     * @param index The index of the player to return
+     * @return The requested player
+     */
+    @Nullable
+    public static Player getPlayer(int index) {
+        if (index >= 0 && index < m_players.size())
+            return m_players.get(index);
+        else
+            return null;
+    }
+}
---
+++ GLET/app/src/main/java/com/sapp/glet/database/Player.java
@@ -0,0 +1,14 @@
+package com.sapp.glet.database;
+
+/**
+ * Created by Simon on 04.03.2017.
+ */
+
+public class Player {
+
+    private boolean m_isOnline = false;
+
+    public boolean isOnline() {
+        return m_isOnline;
+    }
+}

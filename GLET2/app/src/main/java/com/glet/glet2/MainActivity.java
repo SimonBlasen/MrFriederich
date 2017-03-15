d7604e8eea1cdd3ecfd130d5269fff3e24093639
SimonBlasen <simon.yzf@t-online.de>
2017-03-15 17:03:21 +0100

    Well shit works now, don't know why and how

--- GLET2/app/src/main/java/com/glet/glet2/MainActivity.java
+++ GLET2/app/src/main/java/com/glet/glet2/MainActivity.java
@@ -1,13 +1,34 @@
 package com.glet.glet2;
 
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.glet.glet2.service.HelloService;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, HelloService.class);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
     }
 }

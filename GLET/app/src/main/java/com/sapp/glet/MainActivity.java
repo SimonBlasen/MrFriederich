32aa924a735c2567caad786bb4d66ac84d40d9cd
Asssasssiino <simondoll.schule@gmail.com>
2017-03-22 20:53:13 +0100

    Nav_menu changed

--- GLET/app/src/main/java/com/sapp/glet/MainActivity.java
+++ GLET/app/src/main/java/com/sapp/glet/MainActivity.java
@@ -267,20 +267,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_start_game) {
+            Intent intent = new Intent(theContext, StartGame.class);
+            theContext.startActivity(intent);
+        } else if (id == R.id.nav_status) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_agora){
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;

b1846445dca2c5bebefcfb35e414841906366611
SimonBlasen <simon.yzf@t-online.de>
2017-04-12 19:56:03 +0200

    Blabla

--- GLET/app/src/main/java/com/sapp/glet/service/Synchronisator.java
+++ GLET/app/src/main/java/com/sapp/glet/service/Synchronisator.java
@@ -50,4 +50,33 @@ public class Synchronisator {
         }
         //return true;
     }
+
+    public static boolean sendFile(String filename)
+    {
+        /*Client client = new Client("m.m-core.eu", 24400);
+        try {
+            client.Connect();
+            byte[] bytes = new byte[message.getMessage().length + 2];
+            bytes[0] = (byte)(message.getId() >> 8);
+            bytes[1] = (byte)(message.getId());
+            for (int i = 0; i < message.getMessage().length; i++)
+            {
+                bytes[i + 2] = message.getMessage()[i];
+            }
+            client.send(bytes);
+
+            int owndId = Database.getOwndId();
+            byte[] ownIdBytes = DatatypesUtils.IntegerToBytes(owndId);
+
+            client.send(new byte[] {0, 8, ownIdBytes[0], ownIdBytes[1],ownIdBytes[2],ownIdBytes[3]});
+
+            client.send(new byte[] {0, 6, ownIdBytes[0], ownIdBytes[1],ownIdBytes[2],ownIdBytes[3]});
+
+            return true;
+        } catch (IOException e) {
+            return false;
+        }*/
+
+        return true;
+    }
 }
